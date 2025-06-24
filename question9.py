f104b7929ea2043c7cd3a5dc9ed76f114d866278
Elazar <e0548448478@gmail.com>
2025-06-24 13:00:47 +0300

    question 9 solved

---
+++ question9.py
@@ -0,0 +1,37 @@
+import csv
+from dataclasses import field
+
+#creating a dictionary from the original file
+try:
+    with open("sample_names.csv","r", encoding="utf-8-sig") as f:
+        reader = csv.DictReader(f)
+        result_dict = {}
+        for row in reader:
+            city = row["City"]
+            title = row["Title"]
+            if city not in result_dict:
+                result_dict[city] = {title: 1}
+            else:
+                if title in result_dict[city]:
+                    result_dict[city][title]+= 1
+                else:
+                    result_dict[city][title] = 1
+except IOError as ex:
+    print("Error performing I/O operations on the file: ",ex)
+
+#writing the dictionary to a new file
+try:
+    with open("new_file.csv", "w", newline='', encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["City","Subject","Count"])
+        for city,subjects in result_dict.items():
+            for subject in subjects:
+                writer.writerow([city,subject,subjects[subject]])
+except IOError as ex:
+    print("Error performing I/O operations on the file: ",ex)
+
+#test the result
+with open("new_file.csv","r", encoding="utf-8-sig") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            print(row)
